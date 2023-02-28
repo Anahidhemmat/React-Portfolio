8baa5daa8a5b7dd89559f1df92d9faa5c87de721
Anahid Hemmat <anahidhemmat2130@gmail.com>
2023-03-01 00:38:54 +0400

    Header component & Navigation & Menu toggle

--- my-app/src/components/header/Header.js
+++ my-app/src/components/header/Header.js
@@ -1,5 +1,65 @@
+import React, { useState } from "react";
+import "./header.css";
+
 const Header = () => {
-  return <div>start</div>;
+  // =========== Toggle Menu =========
+  const [Toggle, toggleMenu] = useState(false);
+  return (
+    <header className="header">
+      <nav className="nav container">
+        <a href="index.html" className="nav-logo">
+          Anna
+        </a>
+        <div className={Toggle ? "nav-menu show-menu" : "nav-menu"}>
+          <ul className="nav-list grid">
+            <li className="nav-item">
+              <a href="" className="nav-link active-link">
+                <i className="uil uil-estate nav-icon"></i>
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="" className="nav-link">
+                <i className="uil uil-user nav-icon"></i>
+                About
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="" className="nav-link">
+                <i className="uil uil-file-alt nav-icon"></i>
+                Skills
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="" className="nav-link">
+                <i className="uil uil-briefcase-alt nav-icon"></i>
+                Services
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="" className="nav-link">
+                <i className="uil uil-scenery nav-icon"></i>
+                Portfolio
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="" className="nav-link">
+                <i className="uil uil-message nav-icon"></i>
+                Contact me
+              </a>
+            </li>
+          </ul>
+          <i
+            className="uil uil-times nav-close"
+            onClick={() => toggleMenu(!Toggle)}
+          ></i>
+        </div>
+        <div className="nav-toggle" onClick={() => toggleMenu(!Toggle)}>
+          <i className="uil uil-apps"></i>
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
